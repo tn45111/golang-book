fb9421b67c194be6b9e59ec7a7c1dc04c432412b
tn45111 <wiphak.ta@tnis.com>
2018-02-08 10:42:29 +0700

    chaptor4-2 add convert temp

--- chaptor4-2/main.go
+++ chaptor4-2/main.go
@@ -4,10 +4,11 @@ import "fmt"
 
 func main() {
 
-	fmt.Printf("Enter a number: ")
-	var input float64
-	fmt.Scanf("%f", &input)
-	output:=input*2
-	fmt.Println(output)
+	fmt.Printf("Convert Farenheit to Celsius Enter temp(F): ")
+	var Farenheit float32
+	var Celsius float32
+	fmt.Scanf("%f", &Farenheit)
+	Celsius=((Farenheit-32)*5)/9
+	fmt.Printf("Temp(C) : %.2f",Celsius)
 
 }
